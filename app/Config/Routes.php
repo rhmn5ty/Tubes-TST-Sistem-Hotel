3b839b105eb852e176a0d33f6140d361aee7b452
rahman_satya <18221117@std.stei.itb.ac.id>
2023-12-08 20:22:46 +0700

    connect database

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/about', 'About::index');
 $routes->get('/blog', 'Blog::index');
+$routes->get('/login', 'LoginController::index');
+$routes->get('/logout', 'LoginController::logout');
+$routes->post('/login_action', 'LoginController::login_action');
---
+++ app/Views/login.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+    <h2>Login</h2>
+    <form action="/login_action" method="POST">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">
+            Signin</button>
+    </form>
+</body>
+
+</html>
